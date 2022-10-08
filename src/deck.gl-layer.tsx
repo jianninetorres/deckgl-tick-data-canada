dbb21816dda3593a4e9b438a98e64998e8410b2f
Jiannine Torres <jianninetorres@gmail.com>
2022-10-08 00:02:16 -0400

    Use heatmap

--- src/deck.gl-layer.tsx
+++ src/deck.gl-layer.tsx
@@ -1,4 +1,5 @@
 import { GeoJsonLayer } from "@deck.gl/layers/typed";
+import { HeatmapLayer } from "@deck.gl/aggregation-layers/typed";
 
 import { DataProps } from "./App";
 
@@ -9,25 +10,13 @@ interface RenderLayersProps {
 
 const RenderLayers = (props: RenderLayersProps) => {
   const layers = [
-    new GeoJsonLayer({
+    new HeatmapLayer({
       id: "tick-data",
       data: props.data,
-      getFillColor: [160, 160, 180, 200],
-      extruded: true,
-      pickable: true, //  identify an object and the layer it's in
-      stroked: false,
-      filled: true,
-      pointRadiusUnits: "pixels",
-      pointType: "circle+text",
-      lineWidthScale: 20,
-      lineWidthMinPixels: 4,
-      getLineColor: [225, 133, 61],
-      getPointRadius: 4,
-      getLineWidth: 5,
-      getElevation: 30,
-      getText: (d: { properties: { tick_species: any } }) =>
-        d.properties.tick_species,
-      getTextSize: 12,
+      getPosition: (d: { geometry: { coordinates: any } }) =>
+        d.geometry.coordinates,
+      getWeight: 10,
+      aggregation: "SUM",
     }),
   ];
 
